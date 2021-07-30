ac0019500da3fee0b8b8d25805df12c5e9110acf
unknown <alexandre.araiza@gmail.com>
2021-07-30 13:00:03 +0200

    Added categories

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,66 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
+
+import * as navigationStyles from "./navigation.module.css";
+
+let activeButton = null;
+let activeGroupElement = null;
+
+function getClickedButtonElement(clickedElement) {
+  let element = clickedElement;
+  while (element.tagName !== "BUTTON") {
+    element = element.parentElement;
+  }
+  return element;
+}
+
+export function expand(event) {
+  if (activeButton) {
+    activeButton.children[1].style.opacity = "";
+    activeGroupElement.style.height = "";
+  }
+  let element = getClickedButtonElement(event.target);
+  if (element === activeButton) {
+    activeButton = null;
+    activeGroupElement = null;
+    return;
+  }
+  activeButton = element;
+  activeGroupElement = activeButton.parentElement.nextElementSibling;
+  activeButton.children[1].style.opacity = 0;
+  activeGroupElement.style.height = activeGroupElement.scrollHeight + "px";
+}
 
 const Navigation = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allCategoriesJson {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  `);
+
+  const allCategories = data.allCategoriesJson.edges;
+
   return (
-    <nav>
-      <Link to="/">Els meus plats</Link>
+    <nav className={navigationStyles.nav}>
+      <div className={navigationStyles.expandGroup}>
+        <div>
+          <Link to="/">Els meus plats</Link>
+          <button className={navigationStyles.expandBtn} onClick={expand}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{ "position": "absolute" }}><path d="M19 13H5v-2h14v2z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={navigationStyles.disappear}><path d="M11 5h2v14h-2z"/></svg>
+          </button>
+        </div>
+        <div className={navigationStyles.linkList}>
+          {allCategories.map((category) => <Link to={"/categories/" + category.node.id}>{category.node.name}</Link>)}
+        </div>
+      </div>
       <Link to="/els-sopars-de-duro">Els sopars de duro</Link>
       <Link to="/qui-soc">Qui soc</Link>
       <Link to="/contacte">Contacte</Link>
---
+++ src/pages/categories/{categoriesJson.id}.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../../components/Layout";
+import Grid from "../../components/grid";
+
+export const query = graphql`
+  query($id: String!) {
+    allRecipesJson(filter: { categories: { eq: $id } }) {
+      edges {
+        node {
+          id
+          image {
+            childImageSharp {
+              gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+            }
+          }
+          title
+        }
+      }
+    }
+    categoriesJson(id: { eq: $id }) {
+      name
+    }
+  }
+`;
+
+const Category = ({ data }) => {
+  const allRecipes = data.allRecipesJson.edges;
+
+  return (
+    <Layout>
+      <h1>{data.categoriesJson.name}</h1>
+      <Grid recipes={allRecipes} />
+    </Layout>
+  );
+}
+
+export default Category;
